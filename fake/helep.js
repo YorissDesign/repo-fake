846f45e62a1a68176ad8574ea032c483775d5bca
YorissDesign <yogamer077@gmail.com>
2022-03-13 13:24:43 +0700

    ouh

---
+++ fake/helep.js
@@ -0,0 +1,376 @@
+///TAMPILAN BY DEFF
+//JANGAN LUPA SUBS YT DEFFBOTZZ
+
+exports.allmenu = () => {
+	return `╭─ 👑「 *OWNER* 」	          
+│≻ ping
+│≻ self
+│≻ ban
+│≻ unban
+│≻ listban
+│≻ public
+│≻ runtime
+│≻ setthumb
+│≻ autoketik
+│≻ autovn
+│≻ autoread
+│≻ addtruth kata
+│≻ adddare kata
+│≻ listbadword
+│≻ setmenu loc
+│≻ setmenu fyt
+│≻ setmenu catalog
+│≻ delbadword kata
+│≻ addbadword kata
+│≻ listpremium
+│≻ addpremium @tag
+│≻ delpremium @rag
+│≻ addpetualangpro @tag
+│≻ delpetualangpto @tag
+╰──── 
+
+╭─ ⚒️「 *TOOLS* 」	
+│≻ jadibot
+│≻ stopjadibot
+│≻ listjadibot
+╰──── 
+
+╭─🗯️「 *STALKER* 」
+│≻  ghstalk username
+╰──── 
+
+╭─ 🛒「 *STORE* 」	
+│≻ buylimit jumlah
+│≻ buyglimit jumlah
+╰────
+ 
+╭─ 🍅「 *VVIBU* 」
+│≻ loli
+│≻ milf
+│≻ waifu
+│≻ waifu2
+│≻ husbu
+│≻ wallml
+│≻ cosplay
+│≻ megumin
+│≻ nekochan
+│≻ nekochan2
+╰──── 
+
+╭─ 🍑「 *NSFW* 」
+│≻ ero
+│≻ ass
+│≻ cum
+│≻ jahy
+│≻ neko
+│≻ pussy
+│≻ thighs
+│≻ hentai
+│≻ manga
+│≻ bdsm
+│≻ blowjob
+│≻ ahegao
+│≻ cuckold
+│≻ trapnime
+│≻ femdom
+│≻ glasses 
+╰──── 
+
+╭─⚡️「 *DOUJINDESU* 」
+│≻ nhentai code
+│≻ doujinkode
+│≻ nuklirkode
+╰──── 
+
+╭─ 🎏「 *STICKER* 」
+│≻ gura 
+│≻ patrick
+│≻ sanime
+╰──── 
+
+╭─ 📼「 *STICKER GIF* 」
+│≻ wink
+╰──── 
+
+╭─ 🔥️「 *GAME* 」	
+│≻ tebakkata
+│≻ tebakanime
+│≻ tebakgambar
+│≻ caklontong
+╰──── 
+
+╭─🍂️「 *RANDOM API* 」
+│≻ fox
+│≻ dog
+│≻ bird
+│≻ koala
+│≻ panda
+│≻ redpanda
+╰──── 
+
+╭─🌿️「 *NULIS* 」
+│≻ nulis teks
+│≻ nuliskiri teks
+│≻ nuliskanan teks
+│≻ foliokiri teks
+│≻ foliokanan teks
+╰──── 
+
+╭─ 🔎「 *SEARCH* 」
+│≻ ssweb link 
+│≻ lirik lirik lagu
+│≻ gimage	query
+│≻ pinterest query
+│≻ ytsearch query
+│≻ ranime animenya
+╰──── 
+
+╭─ 📥「 *DOWLOAD* 」	
+│≻ play query
+│≻ tiktok link
+│≻ ytmp3 link
+│≻ ytmp4 link
+│≻ twitter link
+│≻ facebook link
+│≻ Instagram link
+│≻ mediafire link
+╰──── 
+
+╭─ 💿「 *MEDIA* 」	
+│≻ attp text
+│≻ tourl img
+│≻ toimg reply
+│≻ semoji emot
+│≻ sticker reply
+╰──── 
+
+╭─ 👑「 *UPSWBOT* 」
+│≻ upswgift
+│≻ upswteks
+│≻ upswvideo
+│≻ upswaudio
+│≻ upswvoice
+│≻ upswlokasi
+│≻ upswimage
+│≻ upswsticker	
+╰──── 
+
+╭─ 🎮「 *FUNTIME* 」	
+│≻ slot
+│≻ truth
+│≻ dare
+│≻ meme
+│≻ jadian
+│≻ ngewe
+│≻ tergay
+│≻ terpodo
+│≻ terjelek
+│≻ terwibu
+│≻ tercantik
+│≻ terganteng
+│≻ darkjokes
+╰──── 
+
+╭─ 🏔️「 *RPG MENU* 」
+│≻ joinrpg
+│≻ quest
+│≻ mining
+│≻ luckyday
+│≻ mancing
+│≻ inventori
+│≻ adventure
+│≻ topleaderboard
+│≻ sellikan
+│≻ sellbesi
+│≻ sellemas
+│≻ selldiamond
+│≻ killslime
+│≻ killgoblin
+│≻ killdevil
+│≻ killbehemoth
+│≻ killdemond
+│≻ killdemondking
+╰──── 
+
+╭─✨「 *GROUP* 」	
+│≻ group
+│≻ hacked
+│≻ tagall 
+│≻ hidetag 
+│≻ add 62
+│≻ linkgroup
+│≻ resetlink
+│≻ kick tag
+│≻ join link
+│≻ demote tag
+│≻ demoteall
+│≻ promote tag
+│≻ promoreall
+│≻ del replymes
+│≻ setdesc text
+│≻ setname name
+│≻ setppgc replyimg
+│≻ nsfw
+│≻ eventgc
+│≻ antilink
+│≻ welcome
+│≻ nobadword
+╰──── 
+
+╭─ ⚙️「 *STORAGE* 」
+│≻ listvn
+│≻ listvideo
+│≻ liststicker
+│≻ listimg
+│≻ addvn nama
+│≻ addvideo nama
+│≻ addsticker nama
+│≻ addimage nama
+│≻ getvn data
+│≻ getvideo data
+│≻ getsticker data
+│≻ getimage data
+╰──── 
+
+╭─📍「 *CONVERT* 」
+│≻ gtts id text
+│≻ tovn replyaudio
+│≻ tomp3 replyvideo
+│≻ fast replyvideo 
+│≻ slow replyvideo 
+│≻ reverse replyvideo 
+│≻ slow2 replyaudio 
+│≻ bass replyaudio 
+│≻ tupai replyaudio
+│≻ gemuk replyaudio 
+│≻ nightcore replyaudio
+╰──── 
+
+╭─ 📖「 *Text Pro Me* 」	
+│≻ blackpink text
+│≻ neon text
+│≻ greenneon text
+│≻ advanceglow text
+│≻ futureneon text
+│≻ sandwriting text
+│≻ sandsummer text
+│≻ sandengraved text
+│≻ metaldark text
+│≻ neonlight text
+│≻ holographic text
+│≻ text1917 text
+│≻ minion text
+│≻ deluxesilver text
+│≻ newyearcard text
+│≻ bloodfrosted text
+│≻ halloween text
+│≻ jokerlogo text
+│≻ fireworksparkle text
+│≻ natureleaves text
+│≻ bokeh text
+│≻ toxic text
+│≻ strawberry text
+│≻ box3d text
+│≻ roadwarning text
+│≻ breakwall text
+│≻ icecold text
+│≻ luxury text
+│≻ cloud text
+│≻ summersand text
+│≻ horrorblood text
+│≻ thunder text
+│≻ pornhub text text
+│≻ glitch text text
+│≻ avenger text text
+│≻ space text text
+│≻ ninjalogo text text
+│≻ marvelstudio text text
+│≻ lionlogo text text
+│≻ wolflogo text text
+│≻ steel3d text text
+│≻ wallgravity text text
+╰──── 
+
+╭─ 🖼️「 *Photo OXY* 」	
+│≻ shadow text
+│≻ cup text
+│≻ cup1 text
+│≻ romance text
+│≻ smoke text
+│≻ burnpaper text
+│≻ lovemessage text
+│≻ undergrass text
+│≻ love text
+│≻ coffe text
+│≻ woodheart text
+│≻ woodenboard text
+│≻ summer3d text
+│≻ wolfmetal text
+│≻ nature3d text
+│≻ underwater text
+│≻ golderrose text
+│≻ summernature text
+│≻ letterleaves text
+│≻ glowingneon text
+│≻ fallleaves text
+│≻ flamming text
+│≻ harrypotter text
+│≻ carvedwood text
+│≻ arcade8bit text text
+│≻ battlefield4 text text
+│≻ pubg text text
+╰──── 
+
+╭─ 📸「 *Photo 360°* 」	
+│≻ wetglass text
+│≻ multicolor3d text
+│≻ watercolor text
+│≻ luxurygold text
+│≻ galaxywallpaper text
+│≻ lighttext text
+│≻ beautifulflower text
+│≻ puppycute text
+│≻ royaltext text
+│≻ heartshaped text
+│≻ birthdaycake text
+│≻ galaxystyle text
+│≻ hologram3d text
+│≻ greenneon text
+│≻ glossychrome text
+│≻ greenbush text
+│≻ metallogo text
+│≻ noeltext text
+│≻ glittergold text
+│≻ textcake text
+│≻ starsnight text
+│≻ wooden3d text
+│≻ textbyname text
+│≻ writegalacy text
+│≻ galaxybat text
+│≻ snow3d text
+│≻ birthdayday text
+│≻ goldplaybutton text
+│≻ silverplaybutton text
+│≻ freefire text
+╰──── 
+
+╭─☠️「 *REPORT* 」
+│≻  reportadmin 
+│≻  reportdev 
+╰──── 
+
+╭─「 *THANKS TO* 」
+│• _My God_
+│• _My parents_
+│• _Febriansyah_
+│• _FadhilGraphy_
+│• _KirrBotz_
+│• _Deffbotz_
+│• _Kahfz_
+│• _Hardianto_
+│• _NdyieBotz_
+│• _Epan < Babu >_
+│• _All Contributors_
+│• _All Subscriber_
+╰──── `
+}
---
+++ fake/sewa.js
@@ -0,0 +1,65 @@
+exports.semwa = () => {
+	return `╭─「 *SEWABOT* 」
+│ *LIST HARGA* 
+│• SEHARI : FREE / GRATIS
+│• SEMINGGU : 3K/MIMGGU
+│• SEBULAN : 5K/PERBULAN
+│• PERMANEN : 25K
+│
+│ *PAYMENT*
+│• DANA : 085849261085
+│• GOPAY : 085849261085
+│• OVO : 085849261085
+│• PULSA M3 : 085849261085
+╰──── `
+}
+
+exports.deffinfox = () => {
+	return `
+📍not a programmer just developing a hobby...
+🎓still in school! i like to learn nodejs, javascript, html
+🌱my goal in 2022 is to make my parents happy
+✨I like watching anime, playing games and fishing
+🗒️good luck for me and for you
+
+🔎Sosial media?
+ • Instagram @febzabotz
+ • facebook @febriansyah
+ • youtube @febzabotz
+
+📬connect with me?
+ • email : febzabotzofc@gmail.com
+ • whatsapp http://wa.me/62858-2257-8327`
+}
+
+exports.allesce = () => {
+	return `*🌱ALL - SCRIPT BY FEBRIANSYAH*
+
+*📍SC CIKOBOT V1*
+_📎Link : https://youtu.be/m8RMdSBwZX4_
+_🌿Author : @Febriansyah_
+
+*📍SC CIKOBOT V2*
+_📎Link : https://youtu.be/O47vZyzYv9s_
+_🌿Author : @Febriansyah_
+
+*📍SC FREE BASE #1*
+_📎Link : https://youtu.be/lP356kRv_e4_
+_🌿Author : @Febriansyah_
+
+*📍SC FREE BASE #2*
+_📎Link : https://youtu.be/6tbi4SZuvjg_
+_🌿Author : @Febriansyah_
+
+*📍SC ZEEBOT V1*
+_📎Link : https://youtu.be/4EWisOAkI2A_
+_🌿Author : @Febriansyah_
+
+*📍SC ZEEBOT V2*
+_📎Link : https://youtu.be/KnVIKuN3Q_w_
+_🌿Author : @Febriansyah_
+
+
+*🌱JANGAN LUPA SUBSCRIBE - FEBZABOTZ*
+https://youtube.com/c/febzabotz_`
+}
